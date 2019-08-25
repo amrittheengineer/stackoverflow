f956d3ed1314f4093ba5c17e5688bcf1357cd1d1
Amrit <amrittheengineer@users.noreply.github.com>
2019-08-25 21:20:50 +0530

    app.js updated for heroku postbuild

--- app.js
+++ app.js
@@ -3,24 +3,19 @@ const app = express();
 const bodyparser = require("body-parser");
 const port = process.env.PORT || 5000;
 const watsonAPI = require("./routes/api/watson/auth");
-const queryAPI = require("./routes/api/query/auth");
+const googleAPI = require("./routes/api/google/auth");
 const stackoverflowAPI = require("./routes/api/stackoverflow/auth");
 
+app.use(require("cors")());
 app.use(express.static(__dirname + "/public"));
 
 app.use(bodyparser.urlencoded({ extended: true }));
 
 app.use("/watson", watsonAPI);
-app.use("/query", queryAPI);
+app.use("/google", googleAPI);
 app.use("/stackoverflow", stackoverflowAPI);
 
-//@type     GET
-//@route    /
-//@desc     To route the main page of the app and to activate the text-similarity server
-//@access   PUBLIC
 app.get("/", (req, res) => {
-  var spawn = require("child_process").spawn;
-  var process = spawn("python", ["./text-similarity/app.py"]);
   res.redirect("/stackoverflow/api/test");
 });
 
