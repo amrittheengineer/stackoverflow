6ebf17c4780b7e07042519bd76a5f29f22b817ec
Amrit <amrittheengineer@users.noreply.github.com>
2020-09-19 14:24:46 +0530

    build directory changed.

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 const bodyparser = require("body-parser");
+const path = require("path");
 const port = process.env.PORT || 5000;
 const watsonAPI = require("./routes/api/watson/auth");
 const googleAPI = require("./routes/api/google/auth");
 const stackoverflowAPI = require("./routes/api/stackoverflow/auth");
 
 app.use(require("cors")());
-app.use(express.static(__dirname + "/ui/build"));
+app.use(express.static(__dirname + "/public"));
 
 app.use(bodyparser.urlencoded({ extended: true }));
 
@@ -16,6 +17,8 @@ app.use("/google", googleAPI);
 app.use("/stackoverflow", stackoverflowAPI);
 
 app.get("/", (req, res) => {
+  // res.sendFile("index.html");
+  // console.log("Sent");
   res.sendFile("index.html");
 });
 
---
+++ public/precache-manifest.971bc0579b422b0db67084f4edd9a632.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25653b483a93b5ed029d44de176ef1bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2d98ed19ea013189216",
+    "url": "/static/css/2.8e1873ca.chunk.css"
+  },
+  {
+    "revision": "25f1a4d1115851adb392",
+    "url": "/static/css/main.5c03d375.chunk.css"
+  },
+  {
+    "revision": "f2d98ed19ea013189216",
+    "url": "/static/js/2.666f5b5f.chunk.js"
+  },
+  {
+    "revision": "fb2d76672e33799d60b969c12d12e3b0",
+    "url": "/static/js/2.666f5b5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25f1a4d1115851adb392",
+    "url": "/static/js/main.98dc529b.chunk.js"
+  },
+  {
+    "revision": "717be7891cc3d466bc4e",
+    "url": "/static/js/runtime-main.b43b082e.js"
+  }
+]);
